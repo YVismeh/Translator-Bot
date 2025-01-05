1af8362847535cc00220f5dd9bb42d551fae18df
YVismeh <Yvisme41148@gmail.com>
2025-01-05 10:20:18 -0600

    update_lang_ja & fr

--- core/main.py
+++ core/main.py
@@ -9,11 +9,25 @@ translator = Translator()
 
 
 
-@bot.message_handler(func=lambda message: True)
-def welcome_message(message):
-   translated_text = translator.translate(message.text, src="en", dest="fa")
-   print (translated_text)
-   bot.reply_to(message, translated_text.text)
+@bot.message_handler(regexp="ja")
+def to_ja_message(message):
+    translated_text = translator.translate(message.text, src="fa", dest="ja")
+    print(translated_text)
+    bot.reply_to(message, translated_text.text)
+
+
+@bot.message_handler(regexp="fr")
+def to_fr_message(message):
+    translated_text = translator.translate(message.text, src="fa", dest="fr")
+    print(translated_text)
+    bot.reply_to(message, translated_text.text)
+
+
+# @bot.message_handler(func=lambda message: True)
+# def welcome_message(message):
+#    translated_text = translator.translate(message.text, src="en", dest="fa")
+#    print (translated_text)
+#    bot.reply_to(message, translated_text.text)
 
 
 
